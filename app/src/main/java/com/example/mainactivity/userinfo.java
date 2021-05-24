b98021c7e28595a585773436281e67e73753d818
Angelo <akusuma05@student.ciputra.ac.id>
2021-05-24 22:08:19 +0700

    Tambah User Info setelah login

--- app/src/main/java/com/example/mainactivity/userinfo.java
+++ app/src/main/java/com/example/mainactivity/userinfo.java
@@ -9,7 +9,12 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,7 +22,10 @@ import android.widget.TextView;
  * create an instance of this fragment.
  */
 public class userinfo extends Fragment {
-    private TextView registerhere_button;
+    private TextView registerhere_button, welcome, nama_user, dont_have_account;
+    private Button login_button, logout_button;
+    private ImageView logo_login;
+    private TextInputLayout input_email_login, input_password_login;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -65,6 +73,53 @@ public class userinfo extends Fragment {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_userinfo, container, false);
         registerhere_button = v.findViewById(R.id.registerhere_button);
+        login_button = v.findViewById(R.id.login_button);
+        logout_button = v.findViewById(R.id.logout_button);
+        welcome = v.findViewById(R.id.welcome);
+        nama_user = v.findViewById(R.id.nama_user);
+        logo_login = v.findViewById(R.id.logo_login);
+        input_email_login = v.findViewById(R.id.input_email_login);
+        input_password_login = v.findViewById(R.id.input_password_login);
+        dont_have_account = v.findViewById(R.id.dont_have_account);
+
+        welcome.setVisibility(View.INVISIBLE);
+        nama_user.setVisibility(View.INVISIBLE);
+        logout_button.setVisibility(View.INVISIBLE);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerhere_button.setVisibility(View.INVISIBLE);
+                login_button.setVisibility(View.INVISIBLE);
+                logout_button.setVisibility(View.INVISIBLE);
+                logo_login.setVisibility(View.INVISIBLE);
+                input_email_login.setVisibility(View.INVISIBLE);
+                input_password_login.setVisibility(View.INVISIBLE);
+                dont_have_account.setVisibility(View.INVISIBLE);
+                welcome.setVisibility(View.VISIBLE);
+                nama_user.setVisibility(View.VISIBLE);
+                logout_button.setVisibility(View.VISIBLE);
+                Toast.makeText(getContext(), "Login Berhasil", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerhere_button.setVisibility(View.VISIBLE);
+                login_button.setVisibility(View.VISIBLE);
+                logout_button.setVisibility(View.VISIBLE);
+                logo_login.setVisibility(View.VISIBLE);
+                input_email_login.setVisibility(View.VISIBLE);
+                input_password_login.setVisibility(View.VISIBLE);
+                dont_have_account.setVisibility(View.VISIBLE);
+                welcome.setVisibility(View.INVISIBLE);
+                nama_user.setVisibility(View.INVISIBLE);
+                logout_button.setVisibility(View.INVISIBLE);
+                Toast.makeText(getContext(), "Logout Berhasil", Toast.LENGTH_LONG).show();
+            }
+        });
+
         registerhere_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
